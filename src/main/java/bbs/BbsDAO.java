0ced8174bd43634addadc0ecf8c16e4730dde200
kokoi1211 <82691822+kokoi1211@users.noreply.github.com>
2021-10-05 18:43:17 +0900

    Add files via upload

---
+++ src/main/java/bbs/BbsDAO.java
@@ -0,0 +1,174 @@
+package bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BbsDAO {
+	private Connection conn; 
+	private PreparedStatement pstmt; 
+	private ResultSet rs; 
+	
+	public BbsDAO()
+	{
+		try
+		{
+			String dbURL = "jdbc:mysql://localhost:3305/BBS"; 
+			//localhost:3305 => 내컴퓨터에 설치된 MySQL, port 3305의 BBS라는 DB에 접속
+			
+			String dbID = "root";
+			String dbPassword = "root1004";
+			Class.forName("com.mysql.jdbc.Driver"); 
+			 //MySQL에 접속할 수 있도록 매개체 역할을 해주는 하나의 라이브러리, JDBC 드라이버 로드
+			
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+			//DB 접속되면 conn객체에 접속정보가 저장
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public String getDate() {
+		String sql = "select now()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; //데이터베이스 오류
+	}
+	
+	//게시글 번호 부여 메소드
+	public int getNext() {
+		//현재 게시글을 내림차순으로 조회하여 가장 마지막 글의 번호를 구한다
+		String sql = "select bbsID from bbs order by bbsID desc";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+			return 1; //첫 번째 게시물인 경우
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	
+	//글쓰기 메소드
+	public int write(String bbsTitle, String userID, String bbsContent) {
+		String sql = "insert into bbs values(?, ?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userID);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1); //글의 유효번호
+			return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; //데이터베이스 오류
+	}
+	//게시글 리스트 메소드
+		public ArrayList<Bbs> getList(int pageNumber){
+			String SQL = "select * from bbs where bbsID < ? and bbsAvailable = 1 order by bbsID desc limit 10";
+			ArrayList<Bbs> list = new ArrayList<Bbs>();
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					Bbs bbs = new Bbs();
+					bbs.setBbsID(rs.getInt(1));
+					bbs.setBbsTitle(rs.getString(2));
+					bbs.setUserID(rs.getString(3));
+					bbs.setBbsDate(rs.getString(4));
+					bbs.setBbsContent(rs.getString(5));
+					bbs.setBbsAvailble(rs.getInt(6));
+					list.add(bbs);
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return list;
+		}
+		
+		//페이징 처리 메소드
+		public boolean nextPage(int pageNumber) {
+			String sql = "select * from bbs where bbsID < ? and bbsAvailable = 1";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					return true;
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return false;
+		}
+		
+		//하나의 게시글을 보는 메소드
+		public Bbs getBbs(int bbsID) {
+			String sql = "select * from bbs where bbsID = ?";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, bbsID);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					Bbs bbs = new Bbs();
+					bbs.setBbsID(rs.getInt(1));
+					bbs.setBbsTitle(rs.getString(2));
+					bbs.setUserID(rs.getString(3));
+					bbs.setBbsDate(rs.getString(4));
+					bbs.setBbsContent(rs.getString(5));
+					bbs.setBbsAvailble(rs.getInt(6));
+					return bbs;
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		//게시글 수정 메소드
+		public int update(int bbsID, String bbsTitle, String bbsContent) {
+			String sql = "update bbs set bbsTitle = ?, bbsContent = ? where bbsID = ?";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, bbsTitle);
+				pstmt.setString(2, bbsContent);
+				pstmt.setInt(3, bbsID);
+				return pstmt.executeUpdate();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return -1; //데이터베이스 오류
+		}
+		
+		//게시글 삭제 메소드
+		public int delete(int bbsID) {
+			//실제 데이터를 삭제하는 것이 아니라 게시글 유효숫자를 '0'으로 수정한다
+			String sql = "update bbs set bbsAvailable = 0 where bbsID = ?";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, bbsID);
+				return pstmt.executeUpdate();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return -1; //데이터베이스 오류 
+		}
+		
+	}
---
+++ src/main/webapp/main.jsp
@@ -0,0 +1,114 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.io.PrintWriter" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width", initial-scale="1">
+	<!-- viewport=화면상의 표시영역, content=모바일 장치들에 맞게 크기조정, initial=초기화면 배율 설정 -->
+	
+<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- 스타일시트로 css폴더의 bootstrap.css파일 사용 -->
+	
+<title>게시판</title>
+</head>
+<body style= "background-color : white;">
+
+	<%
+		String userID = null;
+		if(session.getAttribute("userID") != null)
+		{
+			userID = (String) session.getAttribute("userID");
+		}
+			//로그인이 된 회원은 로그인의 정보를 담을수 있도록 설정  
+	%>
+
+
+
+	<nav class="navbar navbar-default"> <!-- navbar-색상(inverse = 검은색, default 22222= 색x) -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed"
+			data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+			aria-expanded="false">
+				<!-- class="navbar-toggle collapsed"=>네비게이션의 화면 출력유무 
+				data-toggle="collapse" : 모바일 상태에서 클릭하면서 메뉴가 나오게 설정 -->
+			
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+					<!-- 아이콘 이미지 -->
+				
+			</button>
+			
+			<a class="navbar-brand" href="main.jsp">JSP 게시판</a>
+				<!-- Bootstrap navbar 기본 메뉴바 -->
+		</div>
+		
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav"> <!-- navbar-nav => 네비게이션 바의 메뉴 -->
+				<li class="active"><a href="main.jsp">메인</a></li>
+				<li><a href="bbs.jsp">게시판</a></li>
+				<!-- 메뉴, 게시판의 main.jsp와 bbs.jsp의 파일로 각각 이동 -->
+			</ul>
+			<%
+				if(userID == null) //로그인이 되어있지 않았을때, 
+				{
+					
+				
+			%>
+			
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+						<a href="#" class="dropdown-toggle"
+							data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">접속하기<span class="caret"></span></a>
+							<!-- 임시의 주소링크 "#"을 기재한다. -->
+							<!-- caret = creates a caret arrow icon (▼) -->
+					
+						<ul class="dropdown-menu">
+							<!-- dropdown-menu : 버튼을 눌렀을때, 생성되는 메뉴(접속하기를 눌렀을때 로그인, 회원가입 메뉴 -->
+					
+							<li><a href="login.jsp">로그인</a></li>
+							<li><a href="join.jsp">회원가입</a></li>
+						</ul>
+					</li>	
+			</ul>
+			<%
+				}else	//로그인이 되었을때
+				{
+			%>	
+							<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+						<a href="#" class="dropdown-toggle"
+							data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">회원관리<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<!-- dropdown-menu : 버튼을 눌렀을때, 생성되는 메뉴(접속하기를 눌렀을때 로그인, 회원가입 메뉴 -->
+							<li><a href="logoutAction.jsp">로그아웃</a></li>
+						</ul>
+					</li>	
+			</ul>	
+			<% 
+				}
+			%>
+			
+			
+		</div>
+	</nav>
+	<!-- 메인 페이지 영역 시작 -->
+	<div class="container">
+		<div class="jumbotron">
+			<div class="container">
+				<h1>웹 사이트 소개</h1>
+				<p> JSP 게시판 웹 사이트 입니다 디자인은 부트스트랩을 이용하여 제작하였습니다.
+					</p>
+				
+			</div>
+		</div>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	
+</body>
+</html>
